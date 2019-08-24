0379e1fb883289ca9e98405d2c3ed3d3d9af2a89
EdwardTylopi <41063054+EdwardTylopi@users.noreply.github.com>
2019-08-24 02:59:12 +0200

    Premier commit
    Ajouts :
    - .gitignore
    - app.js
    - package.json
    - package-lock.json

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express'),
+	bodyParser = require('body-parser'),
+	methodOverride = require('method-override'),
+	mongoose = require('mongoose'),
+	port = process.env.PORT || 3000
+
+mongoose.connect('mongodb+srv://EdwardT:24e06t1998@trukafer-traoj.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true})
+	.then(() => {
+		console.log('Connecté à la base de données')
+	})
+
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+
+app.set('view engine', 'pug')
+
+const indexRoute = require('./routes/index')
+app.use('/', indexRoute)
+
+app.listen(port, () => console.log(`Serveur lancé sur le port ${port}`))
